c25612c3017d18a486e3d215fe503bed1ac868c6
cori <coristeiner27@gmail.com>
2021-01-05 17:54:22 -0600

    created first 5 comps

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { WishlistComponent } from './wishlist/wishlist.component';
+import { LibraryComponent } from './library/library.component';
+import { LibrarianComponent } from './librarian/librarian.component';
+import { FindBookComponent } from './find-book/find-book.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WishlistComponent,
+    LibraryComponent,
+    LibrarianComponent,
+    FindBookComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
