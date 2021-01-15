6a582443bcd8570f39de9c6185c4a570c0da5cae
cori <coristeiner27@gmail.com>
2021-01-15 16:51:42 -0600

    trying to figure out how to center the title

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -4,8 +4,10 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
+
 })
 export class HeaderComponent implements OnInit {
+collapsed=true;
 
   constructor() { }
 
